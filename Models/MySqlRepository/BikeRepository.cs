9d813b41024adf67f92b4b69493be82ced8786ca
mironicagicu <mironicagicu@gmail.com>
2020-03-05 01:24:18 +0100

    [Fix] Repostory Pattern made generic

---
+++ Models/MySqlRepository/BikeRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvertismentPlatform.Models.MySqlRepository
+{
+    public class BikeRepository : BaseRepository<BikeItem>, IBikeItemRepository
+    {
+        public BikeRepository(AppDbContext context) : base(context)
+        {
+            
+        }
+
+        public override async Task<BikeItem> GetById(int id)
+        {
+            var item = await context.Set<BikeItem>().FindAsync(id);
+            return item;
+        }
+
+        public override async Task<IEnumerable<BikeItem>> GetAll()
+        {
+            return await context.BikeItems
+                .Include(it => it.Advertisment)
+                    .ThenInclude(ad => ad.ApplicationUser)
+                .ToListAsync();
+        }
+    }
+}
