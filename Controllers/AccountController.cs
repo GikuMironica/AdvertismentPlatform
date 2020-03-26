419f33f2e7a43649e3357f562fdd95b494d8a7bd
mironicagicu <mironicagicu@gmail.com>
2020-03-26 20:32:07 +0100

    Change Password

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -407,6 +407,50 @@ namespace AdvertismentPlatform.Controllers
         }
 
         [HttpGet]
+        public IActionResult ChangePassword(string token, string email)
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // check if user is logged in
+                var user = await userManager.GetUserAsync(User);
+                if (user == null)
+                {
+                    return RedirectToAction("Login");
+                }
+
+                // ChangePasswordAsync changes the user password
+                var result = await userManager.ChangePasswordAsync(user,
+                    model.CurrentPassword, model.NewPassword);
+
+                // The new password did not meet the complexity rules or
+                // the current password is incorrect. Add these errors to
+                // the ModelState and rerender ChangePassword view
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return View();
+                }
+
+                // Upon successfully changing the password refresh sign-in cookie
+                await signInManager.RefreshSignInAsync(user);
+                return View("ChangePasswordConfirmation");
+            }
+
+            return View(model);
+        }
+
+
+        [HttpGet]
         [AllowAnonymous]
         public IActionResult AccessDenied()
         {
