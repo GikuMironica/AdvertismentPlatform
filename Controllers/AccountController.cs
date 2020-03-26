82832613a31be97854acc2023ccc6caa5ef49577
mironicagicu <mironicagicu@gmail.com>
2020-03-26 20:40:46 +0100

    Add password forto local account linked to External Provider ( Google, FaceBook)

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -407,8 +407,17 @@ namespace AdvertismentPlatform.Controllers
         }
 
         [HttpGet]
-        public IActionResult ChangePassword(string token, string email)
+        public async Task<IActionResult> ChangePassword(string token, string email)
         {
+            var user = await userManager.GetUserAsync(User);
+
+            var userHasPassword = await userManager.HasPasswordAsync(user);
+
+            if (!userHasPassword)
+            {
+                return RedirectToAction("AddPassword");
+            }
+
             return View();
         }
 
@@ -444,6 +453,48 @@ namespace AdvertismentPlatform.Controllers
                 // Upon successfully changing the password refresh sign-in cookie
                 await signInManager.RefreshSignInAsync(user);
                 return View("ChangePasswordConfirmation");
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddPassword()
+        {
+            var user = await userManager.GetUserAsync(User);
+
+            var userHasPassword = await userManager.HasPasswordAsync(user);
+
+            if (userHasPassword)
+            {
+                return RedirectToAction("ChangePassword");
+            }
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddPassword(AddPasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await userManager.GetUserAsync(User);
+
+                var result = await userManager.AddPasswordAsync(user, model.NewPassword);
+
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return View();
+                }
+
+                await signInManager.RefreshSignInAsync(user);
+
+                return View("AddPasswordConfirmation");
             }
 
             return View(model);
