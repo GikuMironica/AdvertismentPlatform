3e15b4d2fb5f717929ad3bb27e87eb72f81d3a79
mironicagicu <mironicagicu@gmail.com>
2020-03-29 01:53:08 +0100

    Move JS scrips to wwwroot

---
+++ wwwroot/js/ToggleElements.js
@@ -0,0 +1,11 @@
+﻿function toggle_visibility(id, category) {
+    var e = document.getElementById(id);
+
+    if (e.style.display == 'block')
+        e.style.display = 'none';
+    else
+        e.style.display = 'block';
+
+    $('.hideble').not('#' + id).hide();
+    $('#selectedItemTypevalue').text(category);
+}
